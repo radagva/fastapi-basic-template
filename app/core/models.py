526afa03d912879d50122c8e111b3742d21c271e
= <angel@wearecapicua.com>
2024-08-02 14:22:05 -0400

    add docker-compose
    remove tasks relationship

--- app/core/models.py
+++ app/core/models.py
@@ -1,45 +1,27 @@
 import uuid
-from typing import List
 from pydantic import UUID4
-from sqlalchemy import UUID, ForeignKey, String, DateTime, func
+from sqlalchemy import UUID, String, DateTime, func
 from datetime import datetime
-from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy.orm import Mapped, mapped_column
 
 from app.config.database import Base
 
 
-class UserModel(Base):
-    __tablename__ = "users"
+class Model(Base):
+    __abstract__ = True
     id: Mapped[UUID4] = mapped_column(
         UUID, index=True, primary_key=True, default=uuid.uuid4
     )
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime, nullable=False, server_default=func.now()
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime, nullable=False, server_default=func.now(), onupdate=datetime.now
+    )
+
+
+class UserModel(Model):
+    __tablename__ = "users"
     name: Mapped[str] = mapped_column(String, nullable=False)
     email: Mapped[str] = mapped_column(String, nullable=False, unique=True)
     password: Mapped[str] = mapped_column(String, nullable=False)
-    created_at: Mapped[datetime] = mapped_column(
-        DateTime, nullable=False, server_default=func.now()
-    )
-    updated_at: Mapped[datetime] = mapped_column(
-        DateTime, nullable=False, server_default=func.now(), onupdate=datetime.now
-    )
-
-    tasks: Mapped[List["TaskModel"]] = relationship("TaskModel", back_populates="user")
-
-
-class TaskModel(Base):
-    __tablename__ = "tasks"
-
-    id: Mapped[UUID4] = mapped_column(
-        UUID, index=True, primary_key=True, default=uuid.uuid4
-    )
-    title: Mapped[str | None] = mapped_column(String, nullable=True)
-    content: Mapped[str] = mapped_column(String)
-    user_id: Mapped[UUID4] = mapped_column(UUID, ForeignKey("users.id"))
-    created_at: Mapped[datetime] = mapped_column(
-        DateTime, nullable=False, server_default=func.now()
-    )
-    updated_at: Mapped[datetime] = mapped_column(
-        DateTime, nullable=False, server_default=func.now(), onupdate=datetime.now
-    )
-
-    user: Mapped[UserModel] = relationship("UserModel", back_populates="tasks")
